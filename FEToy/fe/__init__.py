f3f9b8ed3ec224bde0256a7a5de2c89a3c846ba7
yangbai90 <yangbai90@outlook.com>
2021-12-19 16:23:25 +0100

    add FEToy

---
+++ FEToy/fe/__init__.py
@@ -0,0 +1,7 @@
+__author__="Yang Bai"
+__copyright__= "Copyright (C) 2021-present by M3 Group"
+__version__ = "1.0"
+__maintainer__ = "Yang Bai"
+__email__ = "yangbai90@outlook.com"
+__status__ = "development"
+__date__ = "Dec 19, 2021"
